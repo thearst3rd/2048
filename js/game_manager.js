7fb9ee595c43714d1d090a66fdf590c13d759261
thearst3rd <thearst3rd@gmail.com>
2016-08-28 21:17:31 -0400

    Changed probablilties of 1's and 3's to stop the abundance of 1's

--- js/game_manager.js
+++ js/game_manager.js
@@ -70,7 +70,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? (Math.random() < .5 ? 0 : 1) : 2;
+    var value = Math.random() < 0.9 ? (Math.random() < .4 ? 0 : 1) : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
